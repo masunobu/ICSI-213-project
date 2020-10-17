013c5ce05c716c16e045a28c2dbc81f59b90414e
masunobu <60934434+masunobu@users.noreply.github.com>
2020-10-17 18:35:58 -0400

    Create Driver.java

---
+++ LinkedString/Driver.java
@@ -0,0 +1,18 @@
+package project2;
+
+import java.io.FileNotFoundException;
+
+/**
+ * create a reference-based implementation on some String operations
+ * @author Naganobu Masuda
+ * @version 1.0
+ */
+
+public class Driver {
+	public static void main(String[] args) throws FileNotFoundException, ListIndexOutOfBoundsException, ListException{
+		/**
+		 * calls method start to start the entire testing process 
+		 */
+		Helper.start();
+	}
+}
