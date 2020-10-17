3a17d8f40a0eef12c9a0ef66ea4aceab50c4728b
masunobu <60934434+masunobu@users.noreply.github.com>
2020-10-17 18:38:42 -0400

    Create Driver.java

---
+++ BinaryTree/Driver.java
@@ -0,0 +1,15 @@
+package project4;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Design an ADT Address book that can be used to maintain contact information of friends and families.
+ * and two references, one for each of the node's children.
+ * @author Naganobu Masuda
+ * @version 1.0
+ */
+public class Driver {
+	public static void main(String[] args) throws FileNotFoundException {
+		Helper.start();
+	}
+}
