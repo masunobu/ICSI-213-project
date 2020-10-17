9befe4c0f50b4006672c8ed13127aeff80aaf468
masunobu <60934434+masunobu@users.noreply.github.com>
2020-10-17 18:33:57 -0400

    Create Driver.java

---
+++ ArrayList/Driver.java
@@ -0,0 +1,18 @@
+package project1;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Create an ADT Bag composed of a list of grocery items and a set of operations on the list.
+ * @author Naganobu Masuda
+ * @version 1.0
+ */
+
+public class Driver {
+	public static void main(String[] args) throws FileNotFoundException, ListIndexOutOfBoundsException, ListException{
+		/**
+		 * calls method start to start the entire testing process 
+		 */
+		Helper.start();
+	}
+}
